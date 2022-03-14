130eb2c16a4f82f71a0429ae37a37d741af329cc
Daniel Hessel <hesseldaniel21@gmail.com>
2022-03-14 15:32:43 -0300

    feat: configuring routes

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,61 @@
-export function handler(_, response) {
-  return response.end("Hello World!");
+import config from "./config.js";
+import { Controller } from "./controller.js";
+import { logger } from "./utils.js";
+
+const controller = new Controller();
+
+async function routes(request, response) {
+  const { method, url } = request;
+
+  if (method === "GET" && url === "/") {
+    response.writeHead(302, {
+      "Location": config.location.home,
+    });
+
+    return response.end();
+  }
+
+  if (method === "GET" && url === "/home") {
+    const { stream } = await controller.getFileStream(config.pages.homeHTML);
+
+    // Padrão do response é text/html
+    // response.writeHead(200, {
+    //   "Content-Type": "text/html"
+    // });
+
+    return stream.pipe(response);
+  }
+
+  if (method === "GET" && url === "/controller") {
+    const { stream } = await controller.getFileStream(config.pages.controllerHTML);
+
+    return stream.pipe(response);
+  }
+
+  if (method === "GET") {
+    const { stream, type } = await controller.getFileStream(url);
+
+    return stream.pipe(response);
+  }
+
+  response.writeHead(404);
+  return response.end();
+}
+
+function handleError(error, response) {
+  if (error.message.includes("ENOENT")) {
+    logger.warn(`Asset not found ${error.stack}`);
+
+    response.writeHead(404);
+    return response.end();
+  }
+
+  logger.error(`Caught error on API ${error.stack}`);
+
+  response.writeHead(500);
+  return response.end();
+}
+
+export function handler(request, response) {
+  return routes(request, response).catch(error => handleError(error, response));
 }

9ef8cd289383bf0c6c461c8f08340f52bb508d68
Daniel Hessel <hesseldaniel21@gmail.com>
2022-03-15 07:15:30 -0300

    feat: planning the tests for the routes layer

--- tests/unit/server/routes.test.js
+++ tests/unit/server/routes.test.js
@@ -1,7 +1,22 @@
-import { jest, expect, describe, test } from "@jest/globals";
+import { jest, expect, describe, test, beforeEach } from "@jest/globals";
+import config from "../../../server/config.js";
 
-describe("#Routes", () => {
-  test("my best", () => {
-    expect(true).toBeTruthy();
+const { pages } = config;
+
+describe("#Routes - test site for api response", () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+    jest.clearAllMocks();
+  });
+
+  test.todo("GET / - should redirect to home page");
+  test.todo(`GET /home - should response with ${pages.homeHTML} file stream`);
+  test.todo(`GET /controller - should response with ${pages.controllerHTML} file stream`);
+  test.todo("GET /file.ext - should response with file stream");
+  test.todo("GET /unknown - given as inexistent route it should response with 404");
+
+  describe("exceptions", () => {
+    test.todo("given inexistent file it should respond with 404");
+    test.todo("given as error it should respond with 500");
   });
 });
